876cc604656dab31a2cb0470b20a2484e786eddd
rawhx_x <achmadhasbil24@gmail.com>
2025-06-06 23:25:03 +0700

    Add Character and Weapon abstract class

---
+++ weapon/Weapon.java
@@ -0,0 +1,23 @@
+public abstract class Weapon {
+    private String name;
+    private int damage;
+
+    public Weapon(String name, int damage) {
+        this.name = name;
+        this.damage = damage;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getDamage() {
+        return damage;
+    }
+
+    public void setDamage(int damage) {
+        this.damage = damage;
+    }
+
+    abstract public void attack(Character target);
+}
