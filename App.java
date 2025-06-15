4bc0573baf8629df1c8d92ead055b59bd3f0b7de
muhfaizrahman <muhammadfaizrahman07@gmail.com>
2025-06-15 14:19:16 +0700

    add: new weapon (double barrel) and better logic of game when vs AI

--- App.java
+++ App.java
@@ -22,10 +22,29 @@ public class App
             
             Player bot = new Player("Cool Guy", 5);
             Player p = new Player(nama, 5);
-            
-            Revolver revolver = new Revolver("Anaconda", 1);
 
-            Game game = new Game(p, bot, revolver);
+            System.out.println("Choose your weapon: ");
+            System.out.println("1. Revolver");
+            System.out.println("2. Double Barrel");
+            Weapon currWeapon;
+            while (true) {
+                int weaponChoice = input.nextInt();
+                input.nextLine();
+                if (weaponChoice == 1) {
+                    currWeapon = new Revolver("Anaconda", 1);
+                    break;
+                }
+                else if (weaponChoice == 2) {
+                    currWeapon = new DoubleBarrel("Double Barrel", 2);
+                    break;
+                }
+                else {
+                    System.out.println("Invalid input!");
+                    continue;
+                }
+            }
+
+            Game game = new Game(p, bot, currWeapon, true);
             game.gameStart();
         }
         else if(respon == 2)
@@ -38,9 +57,28 @@ public class App
             String namaP2 = input.nextLine();
             Player p2 = new Player(namaP2, 5);
 
-            Revolver revolver = new Revolver("Anaconda", 1);
+            System.out.println("Make a deal! Which weapon will you bring for the duel: ");
+            System.out.println("1. Revolver");
+            System.out.println("2. Double Barrel");
+            Weapon currWeapon;
+            while (true) {
+                int weaponChoice = input.nextInt();
+                input.nextLine();
+                if (weaponChoice == 1) {
+                    currWeapon = new Revolver("Anaconda", 1);
+                    break;
+                }
+                else if (weaponChoice == 2) {
+                    currWeapon = new DoubleBarrel("Double Barrel", 2);
+                    break;
+                }
+                else {
+                    System.out.println("Invalid input!");
+                    continue;
+                }
+            }
 
-            Game game = new Game(p1, p2, revolver);
+            Game game = new Game(p1, p2, currWeapon, false);
             game.gameStart();
         }
         else
--- game/Game.java
+++ game/Game.java
@@ -3,7 +3,7 @@ package game;
 import character.*;
 import weapon.*;
 import java.util.Scanner;
-import java.util.Random;;
+import java.util.Random;
 
 public class Game 
 {
@@ -13,12 +13,14 @@ public class Game
     private Player player1;
     private Player player2;
     private Weapon weapon;
+    private boolean isSinglePlayer;
 
-    public Game(Player player1, Player player2, Weapon weapon)
+    public Game(Player player1, Player player2, Weapon weapon, boolean isSinglePlayer)
     {
         this.player1 = player1;
         this.player2 = player2;
         this.weapon = weapon;
+        this.isSinglePlayer = isSinglePlayer;
     }
 
     public void gameStart()
@@ -38,17 +40,20 @@ public class Game
 
             boolean player1Turn = rand.nextBoolean();
 
-            while(player1.getHealth() > 0 || player2.getHealth() > 0)
+            while(player1.getHealth() > 0 && player2.getHealth() > 0)
             {
                 Player playerNow;
+                Player otherPlayer;
 
                 if(player1Turn == true)
                 {
                     playerNow = player1;
+                    otherPlayer = player2;
                 }
                 else
                 {
                     playerNow = player2;
+                    otherPlayer = player1;
                 }
                 
                 System.out.println("It's " + playerNow.getName() + "'s turn!");
@@ -57,22 +62,37 @@ public class Game
                 showStatus(player1, player2);
                 Thread.sleep(1500);
 
-                while(true)
+                // Kalo single player, AI otomatis attack
+                if (isSinglePlayer && playerNow.getName().equals("Cool Guy")) 
                 {
-                    System.out.println("\n Yo " + playerNow.getName() + " input a number");
-                    System.out.println("1. Life is all about gamble!!");
-
-                    int playerInput = input.nextInt();
-
-                    if(playerInput == 1)
+                    System.out.println("\nCool Guy is making its move...");
+                    Thread.sleep(1500);
+                    weapon.attack(otherPlayer);
+                } 
+                else {
+                    while(true)
                     {
-                        weapon.attack(playerNow);
-                        break;
+                        System.out.println("\n Yo " + playerNow.getName() + " input a number");
+                        System.out.println("1. Life is all about gamble!!");
+
+                        int playerInput = input.nextInt();
+
+                        if(playerInput == 1)
+                        {
+                            weapon.attack(otherPlayer);
+                            break;
+                        }
+                        else
+                        {
+                            System.out.println("invalid input, just throw your life on the table " + playerNow.getName() + " my guy");
+                        }
                     }
-                    else
-                    {
-                        System.out.println("invalid input, just throw your life on the table " + playerNow.getName() + " my guy");
-                    }
+                }
+
+                // Kalau salah satu player mati, game over
+                if (otherPlayer.getHealth() <= 0) {
+                    System.out.println("\nGame Over! " + playerNow.getName() + " wins!");
+                    break;
                 }
 
                 player1Turn = !player1Turn;
@@ -88,14 +108,14 @@ public class Game
     {
         System.out.println("Player 1");
         System.out.println("+---------------+");
-        System.out.printf("| %-4s | %-5s |\n", "Nama", "Health");
+        System.out.printf("| %-4s | %-5s |\n", "Name", "Health");
         System.out.println("+---------------+");
         System.out.printf("| %-5s | %-5d |\n", p1.getName(), p1.getHealth());
         System.out.println("+---------------+");
 
         System.out.println("\nPlayer 2");
         System.out.println("+---------------+");
-        System.out.printf("| %-4s | %-5s |\n", "Nama", "Health");
+        System.out.printf("| %-4s | %-5s |\n", "Name", "Health");
         System.out.println("+---------------+");
         System.out.printf("| %-5s | %-5d |\n", p2.getName(), p2.getHealth());
         System.out.println("+---------------+");
